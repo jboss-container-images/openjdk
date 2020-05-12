5ab966de35a5744482e50210a770b378675e6277
Jonathan Dowland <jdowland@redhat.com>
2020-05-12 11:04:43 +0100

    gendocs: generate image ENV var documentation
    gendocs.py is a rudimentary Python script that operates on the output of
    a cekit build (including a dry-run) and generates AsciiDoc
    Environment-variable documentation for the image.
    
    gendocs.sh is a harness to invoke gendocs.py for every image and version
    we care about: it switches to different git branches and tags, collects
    the image descriptors and runs gendocs.py.
    
    Add a GitHub workflow to invoke gendocs.sh and upload the results to
    GitHub Pages. Trigger on pushes to the ubi8 or ubi9 branches, or a tag
    matching the pattern for releases.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

---
+++ gendocs.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script arranges to call gendocs.py for each image descriptor we
+# wish to generate documentation for, by switching to the relevant git
+# branches and tags and enumerating the image descriptors.
+# It is expected that this script is invoked via GitHub Actions.
+
+engine=${engine-podman}
+
+die()
+{
+    echo "$@"
+    exit 1
+}
+
+addToIndex()
+{
+    echo -e "$@" >> "$workdir/README.adoc"
+}
+
+# generate docs for a single image
+genImageDocs()
+{
+    ref="$1"   # e.g. ubi8
+    input="$2" # e.g. ubi8-openjdk-11.yaml
+
+    name="$(awk '-F"' '/name: &name/ { print $2 }' "$input")"
+    output="docs/$ref/${input/yaml/adoc}"
+
+    mkdir -p "$(dirname "$output")"
+    cekit --descriptor "$input" build --dry-run "$engine"
+    python3 "$workdir/gendocs.py" "$name" > "$output"
+    asciidoctor "$output"
+
+    addToIndex "* link:$ref/${name/\//-}.html[$name]"
+}
+
+# moves to the git ref, enumerates all images, calls
+# genImageDocs for each
+handleRef()
+{
+    ref="$1"
+    git checkout "$ref"
+    for y in ubi?-*yaml; do
+        genImageDocs "$ref" "$y"
+    done
+}
+
+##############################################################################
+
+for dep in cekit python3 asciidoctor; do
+    if ! which "$dep" >/dev/null; then
+        die "$dep is required to execute this script"
+    fi
+done
+
+# This ensures the directory won't be purged when we switch refs
+mkdir -p docs
+touch docs/index.html
+
+# backup files from this ref we need prior to switching git refs
+# (we don't bother with cleaning up workdir, GHA will do that for us)
+workdir="$(mktemp -td gendocs.XXXXXX)"
+cp ./gendocs.py "$workdir/gendocs.py"
+cp ./docs/README.adoc "$workdir/README.adoc"
+
+# documentation for development branches
+addToIndex "\n== Development branches ==\n"
+for branch in ubi8 ubi9; do
+    addToIndex "\n=== $branch ===\n"
+    handleRef "$branch" "$branch"
+done
+
+# documentation for tagged releases
+addToIndex "\n== Released images =="
+for tag in $(git tag -l 'ubi?-openjdk-containers*' | sort -r); do
+    addToIndex "\n=== $tag ===\n"
+    handleRef "$tag"
+done
+
+asciidoctor "$workdir/README.adoc" -o docs/index.html
