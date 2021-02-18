a6fceef6e8531fd396f9f5efea5365f1eb874806
Jayashree Huttanagoudar <jhuttana@redhat.com>
2021-02-18 21:04:40 +0530

    Added changes to produce JRE Headless Image
    Signed-off-by: Jayashree Huttanagoudar <jhuttana@redhat.com>

---
+++ modules/jre/8/configure.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Configure module
+set -e
+
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+
+chown -R jboss:root $SCRIPT_DIR
+chmod -R ug+rwX $SCRIPT_DIR
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/openjdk/jre/*
+
+pushd ${ARTIFACTS_DIR}
+cp -pr * /
+popd
+
+# Set this JDK as the alternative in use
+_arch="$(uname -i)"
+alternatives --set java java-1.8.0-openjdk.${_arch}
