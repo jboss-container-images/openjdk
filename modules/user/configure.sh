0d1ce6eee86006a136bed2efd7e423ee0519ee12
Jayashree Huttanagoudar <jhuttana@redhat.com>
2022-01-05 00:40:36 +0530

    Moved chmod instructions for $HOME (/home/jboss) from nss-wrapper module to user module
    Signed-off-by: Jayashree Huttanagoudar <jhuttana@redhat.com>

--- modules/user/configure.sh
+++ modules/user/configure.sh
@@ -6,6 +6,7 @@ set -e
 # This ID is registered static ID for the JBoss EAP product
 # on RHEL which makes it safe to use.
 groupadd -r jboss -g 185 && useradd -u 185 -r -g root -G jboss -m -d /home/jboss -s /sbin/nologin -c "JBoss user" jboss
+chmod ug+rwX /home/jboss
 
 # OPENJDK-533: Some container runtimes (Docker) will fail to start if
 # the running UID cannot chdir to $HOME
