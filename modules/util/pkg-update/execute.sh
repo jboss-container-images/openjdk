0bbe29b17c53c0b09c88e3df93342c6740c9ed35
Jonathan Dowland <jdowland@redhat.com>
2022-12-22 11:38:57 +0000

    [OPENJDK-1335] ensure microdnf update install_weak_deps=0
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/util/pkg-update/execute.sh
+++ modules/util/pkg-update/execute.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 for candidate in yum dnf microdnf; do
     if command -v "$candidate"; then
         mgr="$(command -v "$candidate")"
-        "$mgr" update -y
+        "$mgr" update --setopt=install_weak_deps=0 --setopt=tsflags=nodocs -y
         "$mgr" -y clean all
         exit
     fi
