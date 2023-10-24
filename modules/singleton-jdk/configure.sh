3b6e49499eb0fb1be8ba94829a780424cce9a23f
Jonathan Dowland <jdowland@redhat.com>
2023-10-24 11:27:58 +0100

    add a comment about syncing alternative group names between modules
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/singleton-jdk/configure.sh
+++ modules/singleton-jdk/configure.sh
@@ -15,7 +15,9 @@ fi
 # our stated JAVA_VERSION-JAVA_VENDOR (e.g.: 11-openjdk; 1.8.0-openj9)
 rpm -e --nodeps $(rpm -qa java-* | grep -v "^java-${JAVA_VERSION}-${JAVA_VENDOR}")
 
-# workaround for https://issues.redhat.com/browse/RHEL-3437
+# workaround for <https://issues.redhat.com/browse/RHEL-3437>
+# The alternative link groups touched here need to match up with those set in
+# modules/jdk/*/configure.sh
 _arch="$(uname -m)"
 for alt in java javac java_sdk_openjdk jre_openjdk; do
   alternatives --set "$alt" "java-${JAVA_VERSION}-${JAVA_VENDOR}.${_arch}"
