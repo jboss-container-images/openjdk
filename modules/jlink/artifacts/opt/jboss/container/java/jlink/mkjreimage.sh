819ae64dd531b5193166de3d5e536a001f09dab8
jmatsuok <jmatsuok@redhat.com>
2023-10-26 12:13:26 -0400

    Addressing review feedback

--- modules/jlink/artifacts/opt/jboss/container/java/jlink/mkjreimage.sh
+++ modules/jlink/artifacts/opt/jboss/container/java/jlink/mkjreimage.sh
@@ -5,10 +5,12 @@ set -euo pipefail
 outdir="${outdir-spring-boot-jre}"
 depsfile="module-deps.txt"
 
-test -f $depsfile
-modules="$(cat $depsfile)"
+function generate_jre_image() {
+	test -f $depsfile
+	modules="$(cat $depsfile)"
 
-$JAVA_HOME/bin/jlink --output "${outdir}/jre" \
-       	--add-modules "$modules" \
-	--strip-debug --no-header-files --no-man-pages \
-	--compress=2
+	$JAVA_HOME/bin/jlink --output "${outdir}/jre" \
+    	   	--add-modules "$modules" \
+		--strip-debug --no-header-files --no-man-pages \
+		--compress=2
+}
