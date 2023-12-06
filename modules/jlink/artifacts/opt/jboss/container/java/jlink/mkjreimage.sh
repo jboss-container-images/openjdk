9433d1a6697fedaa30a14f9072187b6c7bed1d29
Jonathan Dowland <jdowland@redhat.com>
2023-12-06 13:00:11 +0000

    Define S2I_JLINK_OUTPUT_PATH for jlinked runtime destination
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/jlink/artifacts/opt/jboss/container/java/jlink/mkjreimage.sh
+++ modules/jlink/artifacts/opt/jboss/container/java/jlink/mkjreimage.sh
@@ -8,7 +8,7 @@ function generate_jre_image() {
 	test -f $depsfile
 	modules="$(cat $depsfile)"
 
-	$JAVA_HOME/bin/jlink --output "jre" \
+	$JAVA_HOME/bin/jlink --output "$S2I_JLINK_OUTPUT_PATH" \
     	   	--add-modules "$modules" \
 		--strip-debug --no-header-files --no-man-pages \
 		--compress=2
