ae652d848c7f498e9c368c3ca0b17a122cdd121d
Jonathan Dowland <jdowland@redhat.com>
2023-12-13 15:37:59 +0000

    Revert "Merge pull request #401 from Josh-Matsuoka/ubi9-jlink"
    This reverts commit 249397bd81f489013ddaa72896959c20809ebee5, reversing
    changes made to 4bc78f0475338e0faecc904bcd957d9cf88f8e8e.

--- modules/jlink/artifacts/opt/jboss/container/java/jlink/generatejdkdeps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function generatejdkdeps() {
-  echo "Generating JDK deps"
-  $JAVA_HOME/bin/java --list-modules > java-modules.txt
-  < java-modules.txt sed "s/\\@.*//" > modules.txt
-  grep -Fx -f stripped-deps.txt modules.txt | tr '\n' ',' | tr -d "[:space:]" > module-deps.txt
-  echo "jdk.zipfs" >> module-deps.txt
-}
