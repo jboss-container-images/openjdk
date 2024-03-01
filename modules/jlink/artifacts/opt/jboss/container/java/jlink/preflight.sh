897a419dd4996728a15d76ba509877f33789b06d
Jonathan Dowland <jdowland@redhat.com>
2024-03-01 08:35:50 +0000

    [OPENJDK-2736] actually fail in preflight if conditions aren't met
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/jlink/artifacts/opt/jboss/container/java/jlink/preflight.sh
+++ modules/jlink/artifacts/opt/jboss/container/java/jlink/preflight.sh
@@ -4,9 +4,11 @@ jlink_preflight_check()
     if [ "$JAVA_VERSION" -lt 11 ]; then
       echo "Jlink integration not available for JDK${JAVA_VERSION}!"
       echo "Jlink integration is only supported for JDK versions 11 and newer."
+      exit 1
     fi
     if [ ! -d /usr/lib/jvm/java/jmods ]; then
       echo "Jlink integration requires the jmods RPM to be installed in the builder image, e.g."
       echo "        microdnf install -y java-${JAVA_VERSION}-openjdk-jmods"
+      exit 1
     fi
 }
